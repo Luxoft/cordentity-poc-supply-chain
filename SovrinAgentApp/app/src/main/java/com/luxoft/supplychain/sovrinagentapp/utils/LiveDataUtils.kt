a09a25b169e63c8d2c8f71eebfe6159c7a03cc09
voddan <dgvodopyan@gmail.com>
2020-01-17 23:24:26 +0300

    Implemented IndyState.kt

--- SovrinAgentApp/app/src/main/java/com/luxoft/supplychain/sovrinagentapp/utils/LiveDataUtils.kt
+++ SovrinAgentApp/app/src/main/java/com/luxoft/supplychain/sovrinagentapp/utils/LiveDataUtils.kt
@@ -20,3 +20,20 @@ fun <T, R: Any> LiveData<T>.mapNotNull(transform: (T) -> R?): LiveData<R> {
 
 fun <T, R> LiveData<T>.switch(getter: (T) -> LiveData<R>): LiveData<R> =
     Transformations.switchMap(this, getter)
+
+fun <T1: Any, T2: Any, R> combine(source1: LiveData<T1>, source2: LiveData<T2>, combinator: (T1, T2) -> R): LiveData<R> {
+    val mediator = MediatorLiveData<R>()
+
+    val updater: () -> Unit = {
+        val v1 = source1.value
+        val v2 = source2.value
+
+        if (v1 != null && v2 != null)
+            mediator.value = combinator(v1, v2)
+    }
+
+    mediator.addSource(source1) { updater() }
+    mediator.addSource(source2) { updater() }
+
+    return mediator
+}
