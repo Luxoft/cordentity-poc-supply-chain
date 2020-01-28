bf3373472750e1e98b9fabbea609aa698a6081f0
voddan <dgvodopyan@gmail.com>
2020-01-28 23:38:18 +0300

    Corrected Agent endpoint for the demo

--- SovrinAgentApp/app/src/main/java/com/luxoft/supplychain/sovrinagentapp/application/AppConfig.kt
+++ SovrinAgentApp/app/src/main/java/com/luxoft/supplychain/sovrinagentapp/application/AppConfig.kt
@@ -1,8 +1,9 @@
 package com.luxoft.supplychain.sovrinagentapp.application
 
 //const val WS_ENDPOINT = "ws://localhost:8094/ws"
-const val WS_ENDPOINT = "ws://13.92.115.239:8094/ws"  // azure
+//const val WS_ENDPOINT = "ws://13.92.115.239:8094/ws"  // azure
 //const val WS_ENDPOINT = "ws://3.17.65.252:8094/ws"  // amazon
+const val WS_ENDPOINT = "ws://52.224.104.135:8094/ws"  // client
 
 const val WS_LOGIN = "medical-supplychain"
 const val WS_PASS = "secretPassword"
