af46b5adf2eda764e899e4f43540a168dbdd07ff
voddan <dgvodopyan@gmail.com>
2020-01-29 16:45:16 +0300

    Abstracted attribute names of known indy schemas

--- SovrinAgentApp/app/src/main/java/com/luxoft/supplychain/sovrinagentapp/data/KnownSchemas.kt
+++ SovrinAgentApp/app/src/main/java/com/luxoft/supplychain/sovrinagentapp/data/KnownSchemas.kt
@@ -1,5 +1,9 @@
 package com.luxoft.supplychain.sovrinagentapp.data
 
+/**
+ * Information about Indy schemas that needs to be known for the app to function.
+ * May need updating after creation of new schemas.
+ * */
 object KnownSchemas {
     object PersonalId {
         val schemaName = "Patient Demographics"
